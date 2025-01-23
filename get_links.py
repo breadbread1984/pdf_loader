4d3de443db616af1b667e3ce8c5c98d8e67d69da
breadbread1984 <breadbread1984@163.com>
2025-01-23 15:18:05 +0800

    updat

--- get_links.py
+++ get_links.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-from absl import flags
+from absl import flags, app
 from os.path import splitext
 import requests
+import string
+import random
 import json
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 
 FLAGS = flags.FLAGS
 
@@ -19,25 +21,28 @@ def generate_random_string(length):
   return random_string
 
 def main(unused_argv):
-  links = dict()
+  all_links = dict()
   try:
     response = requests.get(FLAGS.url)
     response.raise_for_status()
     response.encoding = response.apparent_encoding
     soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.find('a')
+    links = soup.find_all('a')
     for link in links:
-      href = link.get("href")
+      url = urljoin(FLAGS.url, link.get("href"))
       text = link.get_text()
       parsed_url = urlparse(url)
       f = parsed_url.path.split('/')[-1]
       stem, ext = splitext(f)
       if ext != '.pdf': continue
-      if text.strip() == '' or text.strip() in lisks:
+      if text.strip() == '' or text.strip() in all_links:
         text += generate_random_string(10)
-      links[text] = href
+      all_links[text] = url
+  except:
+    print('failed to access given url!')
+    raise
   with open(FLAGS.output, 'w') as f:
-    f.write(json.dumps(links, indent = 2))
+    f.write(json.dumps(all_links, indent = 2))
 
 if __name__ == "__main__":
   add_options()
