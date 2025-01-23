cbff247eaed40bd2eb48b7b85bdcff2cc1fe58e8
breadbread1984 <breadbread1984@163.com>
2025-01-23 12:04:44 +0800

    update

--- download.py
+++ download.py
@@ -25,6 +25,7 @@ def add_options():
   flags.DEFINE_string('psql_password', default = 'igs', help = 'postgresql password')
   flags.DEFINE_integer('retry', default = 5, help = 'retry times')
   flags.DEFINE_integer('workers', default = 32, help = 'number of workers')
+  flags.DEFINE_boolean('new', default = False, help = 'whether the download task is new')
 
 def main(unused_argv):
   # download all pdfs
@@ -32,35 +33,50 @@ def main(unused_argv):
   mkdir(FLAGS.download_path)
   with open(FLAGS.input_json, 'r') as f:
     pdfs = json.loads(f.read())
-  download_tasks = list()
-  for description, pdf_list in pdfs.items():
-    for _, url in pdf_list.items():
-      download_tasks.append((url, FLAGS.download_path, description, FLAGS.retry))
-  with concurrent.futures.ThreadPoolExecutor(max_workers = FLAGS.workers) as executor:
-    results = list(executor.map(download, download_tasks))
-  failed_results = list(filter(lambda x: x['output_path'] is None, results))
-  succeed_results = list(filter(lambda x: x['output_path'] is not None, results))
-  with open('download_failed.json', 'w') as f:
-    f.write(json.dumps(failed_results))
+  if FLAGS.new:
+    rmtree('download_succeed.json')
+    rmtree('minio_succeed.json')
+  if not exists('download_succeed.json'):
+    download_tasks = list()
+    for description, pdf_list in pdfs.items():
+      for _, url in pdf_list.items():
+        download_tasks.append((url, FLAGS.download_path, description, FLAGS.retry))
+    with concurrent.futures.ThreadPoolExecutor(max_workers = FLAGS.workers) as executor:
+      results = list(executor.map(download, download_tasks))
+    failed_results = list(filter(lambda x: x['output_path'] is None, results))
+    succeed_results = list(filter(lambda x: x['output_path'] is not None, results))
+    with open('download_failed.json', 'w') as f:
+      f.write(json.dumps(failed_results))
+    with open('download_succeed.json', 'w') as f:
+      f.write(json.dumps(succeed_results))
+  else:
+    with open('download_succeed.json', 'r') as f:
+      succeed_results = json.loads(f.read())
   # add to object storing serivce
-  minio_tasks = list()
-  for file_detail in succeed_results:
-    minio_tasks.append((
-      FLAGS.minio_host,
-      FLAGS.minio_user,
-      FLAGS.minio_password,
-      FLAGS.minio_bucket,
-      file_detail['output_path'],
-      file_detail['url'],
-      file_detail['filename'],
-      file_detail['description']
-    ))
-  with concurrent.futures.ThreadPoolExecutor(max_workers = FLAGS.workers) as executor:
-    results = list(executor.map(minio_upload, minio_tasks))
-  failed_results = list(filter(lambda x: x['minio_url'] is None, results))
-  succeed_results = list(filter(lambda x: x['minio_url'] is not None, results))
-  with open('minio_failed.json', 'w') as f:
-    f.write(json.dumps(failed_results))
+  if not exists('minio_succeed.json'):
+    minio_tasks = list()
+    for file_detail in succeed_results:
+      minio_tasks.append((
+        FLAGS.minio_host,
+        FLAGS.minio_user,
+        FLAGS.minio_password,
+        FLAGS.minio_bucket,
+        file_detail['output_path'],
+        file_detail['url'],
+        file_detail['filename'],
+        file_detail['description']
+      ))
+    with concurrent.futures.ThreadPoolExecutor(max_workers = FLAGS.workers) as executor:
+      results = list(executor.map(minio_upload, minio_tasks))
+    failed_results = list(filter(lambda x: x['minio_url'] is None, results))
+    succeed_results = list(filter(lambda x: x['minio_url'] is not None, results))
+    with open('minio_failed.json', 'w') as f:
+      f.write(json.dumps(failed_results))
+    with open('minio_succeed.json', 'w') as f:
+      f.write(json.dumps(succeed_results))
+  else:
+    with open('minio_succeed.json', 'r') as f:
+      succeed_results = json.loads(f.read())
   # add to sql database
   conn = psycopg2.connect(
     database = FLAGS.psql_db,
